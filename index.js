98a311df56a011f00b1fb93c2d5e56239ff8ad17
Neelima1220 <95346804+Neelima1220@users.noreply.github.com>
2022-10-01 09:16:18 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// generate OTP
+
+function generateOtp() {
+  const x = '0123456789';
+  const otp = [];
+  for (let i = 0; i < 5; i++) {
+    let randomNub = Math.floor(Math.random() * x.length);
+    otp.push(x[randomNub]);
+  }
+  console.log(otp.join(''));
+  return otp.join('');
+}
+
+generateOtp();
+generateOtp();
+generateOtp();
+generateOtp();
+generateOtp();
+generateOtp();
+generateOtp();
